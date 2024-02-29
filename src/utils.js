a6e774ea716760a09e8d1fa535c18308c7934866
wwwdi <www.dibyanshukiro007@gmail.com>
2024-03-01 00:33:08 +0530

    navbar

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
 export const getImageUrl = (path) => {
-    return new URL('assets/${path}',import.meta.url).href;  //dynamically import images to website
+  return new URL(`/assets/${path}`, import.meta.url).href; //dynamically import images to website
 };
